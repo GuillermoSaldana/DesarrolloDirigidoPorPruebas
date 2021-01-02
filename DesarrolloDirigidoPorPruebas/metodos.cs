6d45220da526130b53c3797bd43e15c06bc9dc57
Guillermo Saldaña Suárez <gss1001@alu.ubu.es>
2021-01-02 23:41:26 +0100

    Aladir test de comprobar URL

--- DesarrolloDirigidoPorPruebas/metodos.cs
+++ DesarrolloDirigidoPorPruebas/metodos.cs
@@ -8,6 +8,82 @@ namespace DesarrolloDirigidoPorPruebas
 {
     public class Metodos
     {
-        
+        List<string> listaEspecial = new List<string>();
+
+        public Boolean comprobarContrasena2(String contrasena)
+        {
+            List<char> lista = contrasena.ToList<char>();
+
+            if (string.IsNullOrEmpty(contrasena))
+            {
+                return false;
+            }
+
+            if (contrasena.Length > 10 && contieneMayuscula(lista) && contieneMinuscula(lista) 
+                && contieneNumeros(lista) && contieneCaracterEspecial(contrasena))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public Boolean contieneMayuscula(List<char> contrasena)
+        {
+            foreach (char a in contrasena)
+            {
+                if (char.IsUpper(a))
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        public Boolean contieneMinuscula(List<char> contrasena)
+        {
+            foreach (char a in contrasena)
+            {
+                if (char.IsLower(a))
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        public Boolean contieneNumeros(List<char> contrasena)
+        {
+            foreach (char a in contrasena)
+            {
+                if (char.IsDigit(a))
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        public Boolean contieneCaracterEspecial(string contrasena)
+        {
+            listaEspecial.Add("-");
+            listaEspecial.Add("_");
+            listaEspecial.Add("+");
+            listaEspecial.Add("*");
+            listaEspecial.Add("#");
+
+            foreach (String a in listaEspecial)
+            {
+                if (contrasena.Contains(a))
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
     }
 }
+
--- DesarrolloDirigidoPorPruebasTests/MetodosTests.cs
+++ DesarrolloDirigidoPorPruebasTests/MetodosTests.cs
@@ -11,7 +11,7 @@ namespace DesarrolloDirigidoPorPruebas.Tests
     [TestClass()]
     public class MetodosTests
     {
-        Metodos metodo;
+        Metodos metodo = new Metodos();
 
         [TestInitialize]
         public void MetodoInicialice()
@@ -32,6 +32,21 @@ namespace DesarrolloDirigidoPorPruebas.Tests
             Assert.IsTrue(metodo.comprobarContrasena2("Password1234*"));
             Assert.IsFalse(metodo.comprobarContrasena2("Password1234"));
             Assert.IsFalse(metodo.comprobarContrasena2(""));
+        }
+
+        [TestMethod()]
+        public void comprobarURLTest()
+        {
+            Assert.IsTrue(metodo.comprobarURL("http://Cosas.org"));
+            Assert.IsTrue(metodo.comprobarURL("https://ubuvirtual.ubu.es"));
+            Assert.IsFalse(metodo.comprobarURL("Htt://malescrito.com"));
+            Assert.IsTrue(metodo.comprobarURL("https://ubuvirtual.ubu.es"));
+            Assert.IsTrue(metodo.comprobarURL("https://twitter.com"));
+            Assert.IsFalse(metodo.comprobarURL("https://twitter.qwe"));
+            Assert.IsTrue(metodo.comprobarURL("https://web.whatsapp.com"));
+            Assert.IsTrue(metodo.comprobarURL("https://ubuvirtual.ubu.es/course"));
+            Assert.IsTrue(metodo.comprobarURL("https://ubuvirtual.ubu.es"));
+            Assert.IsFalse(metodo.comprobarURL(""));
         }
     }
 }
