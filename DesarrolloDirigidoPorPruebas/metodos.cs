c255e7a41e19aabf11634142dbae24b4534d285c
Carlos Ortúñez Rojo <cor1001@alu.ubu.es>
2021-01-03 03:54:29 +0100

    Metodos Carlos

--- DesarrolloDirigidoPorPruebas/metodos.cs
+++ DesarrolloDirigidoPorPruebas/metodos.cs
@@ -3,12 +3,179 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
 
 namespace DesarrolloDirigidoPorPruebas
 {
     public class Metodos
     {
         List<string> listaEspecial = new List<string>();
+        
+        public Boolean comprobarCorreoElectronico(String correo)
+        {
+            return Regex.IsMatch(correo, @"\A[a-zA-Z0-9\-_]+@[a-zA-Z0-9-]+\.[a-zA-Z]{2,3}\Z");
+        }
+
+        public Boolean comprobarNIF(String NIF)
+        {
+            List<char> control = new List<char>() { 'T', 'R', 'W', 'A', 'G', 'M', 'Y', 'F', 'P', 'D', 'X', 'B', 'N', 'I', 'Z', 'S', 'Q', 'V', 'H', 'L', 'C', 'K', 'E'};
+            Boolean sol1 = Regex.IsMatch(NIF, @"\A[0-9]{8}[A-Z]{1}\Z");
+            Boolean sol2 = Regex.IsMatch(NIF, @"\A[KLMXYZ]{1}[0-9]{7}[A-Z]{1}\Z");
+            if(sol1 == true){
+                int total = 0;
+                for (int i = 0; i < 8; i++)
+                {
+                    total += Convert.ToInt32(Char.GetNumericValue(NIF[i]));
+                }
+                int numControl = total % 23;
+                return control[numControl] == NIF[8];
+            }else if(sol2 == true)
+            {
+                int total = 0;
+                for (int i = 1; i < 8; i++)
+                {
+                    total += Convert.ToInt32(Char.GetNumericValue(NIF[i]));
+                }
+                int numControl = total % 23;
+                return control[numControl] == NIF[8];
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public Boolean comprobarNIE(String NIE)
+        {
+            List<char> control = new List<char>() { 'T', 'R', 'W', 'A', 'G', 'M', 'Y', 'F', 'P', 'D', 'X', 'B', 'N', 'I', 'Z', 'S', 'Q', 'V', 'H', 'L', 'C', 'K', 'E' };
+            Boolean sol = Regex.IsMatch(NIE, @"\A[MXYZ]{1}[0-9]{7}[A-Z]{1}\Z");
+            int total = 0;
+            if(sol == true){
+                for (int i = 1; i < 8; i++)
+                {
+                    total += Convert.ToInt32(Char.GetNumericValue(NIE[i]));
+                }
+                int numControl = total % 23;
+                return control[numControl] == NIE[8];
+            }
+            return false;
+
+        }
+
+        public Boolean comprobarCIF(String CIF)
+        {
+            List<char> control = new List<char>() { 'J', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J' };
+            Boolean sol = Regex.IsMatch(CIF, @"\A[ABCDEFGHKLMNPQSX]{1}[0-9]{7}[A-Z0-9]{1}\Z");
+            if(sol == true)
+            {
+                var rgx = new Regex(@"^[0-9]{2}$");
+                int totalPar = 0;
+                int totalImpar = 0;
+                for (int i = 2; i < 8; i+=2)
+                {
+                    totalPar += Convert.ToInt32(Char.GetNumericValue(CIF[i]));
+                }
+                int num;
+                for (int i = 1; i < 8; i+=2)
+                {
+                    num = Convert.ToInt32(Char.GetNumericValue(CIF[i])) * 2;
+                    if (rgx.IsMatch(Convert.ToString(num))) 
+                    {
+                        int decenas = num / 10;
+                        int unidades = num % 10;
+                        num = decenas + unidades;
+                    }
+                    totalImpar += num;
+                }
+                int totalFinal = totalPar + totalImpar;
+                totalFinal = totalFinal % 10;
+                int numControl = 10 - totalFinal;
+                if (CIF[0]=='K' || CIF[0] == 'P' || CIF[0] == 'Q' || CIF[0] == 'S')
+                {
+                    return control[numControl] == CIF[8];
+                }
+                else
+                {
+                    return numControl == Convert.ToInt32(Char.GetNumericValue(CIF[8]));
+                }
+            }
+            return false;
+        }
+
+        public Boolean comprobarCodigoPostal(String CodigoPostal)
+        {
+            return Regex.IsMatch(CodigoPostal, @"\A(^[1-4]{1}[0-9]{4}$)|(^[0]{1}[1-9]{1}[0-9]{3}$)|(^[5]{1}[0-2]{1}[0-9]{3}$)\Z");
+        }
+
+        public Boolean comprobarIBAN(String IBAN)
+        {
+            Boolean sol = Regex.IsMatch(IBAN, @"\AES[0-9]{2}\s[0-9]{4}\s[0-9]{4}\s[0-9]{2}\s[0-9]{10}\Z");
+            if (sol == true)
+            {
+                String IBANsinespacio = IBAN.Replace(" ", "");
+                String num = IBANsinespacio.Substring(4, 20);
+                num += "142800";
+                decimal resto = Convert.ToDecimal(num) % 97;
+                decimal final = 98 - resto;
+                return final == Convert.ToDecimal(IBANsinespacio.Substring(2, 2));
+            }
+            return false;
+        }
+
+        public Boolean comprobarIPv4(String IPv4)
+        {
+            Boolean sol = Regex.IsMatch(IPv4, @"\A[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\Z");
+            String[] partes = IPv4.Split('.');
+            if (sol == true)
+            {
+                foreach (String parte in partes)
+                {
+                    if (Convert.ToInt32(parte) > 255)
+                    {
+                        return false;
+                    }
+                }
+                if(Convert.ToInt32(partes[0])==127 && Convert.ToInt32(partes[1]) != 0 && Convert.ToInt32(partes[2]) != 0 && Convert.ToInt32(partes[3]) != 0 ){
+                    return false;
+                }
+                if (Convert.ToInt32(partes[0]) == 191 && Convert.ToInt32(partes[1]) == 255 && Convert.ToInt32(partes[2]) != 0 && Convert.ToInt32(partes[3]) != 0)
+                {
+                    return false;
+                }
+                if (Convert.ToInt32(partes[0]) == 223 && Convert.ToInt32(partes[1]) == 255 && Convert.ToInt32(partes[2]) == 255 && Convert.ToInt32(partes[3]) != 0)
+                {
+                    return false;
+                }
+                if (Convert.ToInt32(partes[0]) > 233 || Convert.ToInt32(partes[0]) < 1)
+                {
+                    return false;
+                }
+                return true;
+            }
+            return false;
+
+        }
+
+        public Boolean comprobarContrasena1(String contrasena)
+        {
+            List<char> lista = contrasena.ToList<char>();
+
+            if (string.IsNullOrEmpty(contrasena))
+            {
+                return false;
+            }
+
+            if (contrasena.Length > 8 && contieneMayuscula(lista) && contieneMinuscula(lista)
+                && contieneNumeros(lista))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
 
         public Boolean comprobarContrasena2(String contrasena)
         {
--- DesarrolloDirigidoPorPruebasTests/MetodosTests.cs
+++ DesarrolloDirigidoPorPruebasTests/MetodosTests.cs
@@ -11,11 +11,136 @@ namespace DesarrolloDirigidoPorPruebas.Tests
     [TestClass()]
     public class MetodosTests
     {
-        Metodos metodo = new Metodos();
 
-        [TestInitialize]
+        Metodos metodo;
+
+       [TestInitialize]
         public void MetodoInicialice()
         {
+            metodo = new Metodos();
+        }
+
+        [TestCleanup]
+        public void CleanUp()
+        {
+            metodo = null;
+        }
+
+        [TestMethod()]
+        public void comprobarCorreoElectronicoTest()
+        {
+            Assert.IsFalse(metodo.comprobarCorreoElectronico("prueba*@gmail.es"));
+            Assert.IsTrue(metodo.comprobarCorreoElectronico("prueba_@gmail.es"));
+            Assert.IsFalse(metodo.comprobarCorreoElectronico("@gmail.es"));
+            Assert.IsFalse(metodo.comprobarCorreoElectronico("pruebagmail.es"));
+            Assert.IsFalse(metodo.comprobarCorreoElectronico("prueba@gmail*.es"));
+            Assert.IsFalse(metodo.comprobarCorreoElectronico("prueba@gmailes"));
+            Assert.IsFalse(metodo.comprobarCorreoElectronico("prueba@gmail.1es"));
+            Assert.IsFalse(metodo.comprobarCorreoElectronico("prueba@gmail.e"));
+            Assert.IsFalse(metodo.comprobarCorreoElectronico("prueba@gmail.eess"));
+            Assert.IsFalse(metodo.comprobarCorreoElectronico("prueba@"));
+            Assert.IsFalse(metodo.comprobarCorreoElectronico("prueba@.es"));
+            Assert.IsFalse(metodo.comprobarCorreoElectronico("prueba@gmail."));
+            Assert.IsFalse(metodo.comprobarCorreoElectronico(""));
+        }
+
+        [TestMethod()]
+        public void comprobarNIFTest()
+        {
+            Assert.IsFalse(metodo.comprobarNIF("000000000T"));
+            Assert.IsFalse(metodo.comprobarNIF("000000T"));
+            Assert.IsFalse(metodo.comprobarNIF("0000000T"));
+            Assert.IsFalse(metodo.comprobarNIF("A0000000T"));
+            Assert.IsTrue(metodo.comprobarNIF("00000000T"));
+            Assert.IsTrue(metodo.comprobarNIF("K0000000T"));
+            Assert.IsFalse(metodo.comprobarNIF("K0000000A"));
+            Assert.IsFalse(metodo.comprobarNIF("00000000A"));
+            Assert.IsFalse(metodo.comprobarNIF("00000000"));
+            Assert.IsFalse(metodo.comprobarNIF("000AB000T"));
+            Assert.IsFalse(metodo.comprobarNIF(""));
+        }
+
+        [TestMethod()]
+        public void comprobarNIETest()
+        {
+            Assert.IsFalse(metodo.comprobarNIE("Y00000000T"));
+            Assert.IsFalse(metodo.comprobarNIE("Y000000T"));
+            Assert.IsFalse(metodo.comprobarNIE("0000000T"));
+            Assert.IsFalse(metodo.comprobarNIE("A0000000T"));
+            Assert.IsTrue(metodo.comprobarNIE("Y0000000T"));
+            Assert.IsFalse(metodo.comprobarNIE("K0000000A"));
+            Assert.IsFalse(metodo.comprobarNIE("Y0000000"));
+            Assert.IsFalse(metodo.comprobarNIE("Y00AB000T"));
+            Assert.IsFalse(metodo.comprobarNIE(""));
+        }
+
+        [TestMethod()]
+        public void comprobarCIFTest()
+        {
+            Assert.IsFalse(metodo.comprobarCIF("P12345678D"));
+            Assert.IsFalse(metodo.comprobarCIF("P123456D"));
+            Assert.IsFalse(metodo.comprobarCIF("1234567D"));
+            Assert.IsFalse(metodo.comprobarCIF("W1234567D"));
+            Assert.IsTrue(metodo.comprobarCIF("P1234567D"));
+            Assert.IsTrue(metodo.comprobarCIF("A12345674"));
+            Assert.IsFalse(metodo.comprobarCIF("P1234567A"));
+            Assert.IsFalse(metodo.comprobarCIF("P1234567"));
+            Assert.IsFalse(metodo.comprobarCIF("P12AB567D"));
+            Assert.IsFalse(metodo.comprobarCIF(""));
+        }
+
+        [TestMethod()]
+        public void comprobarCodigoPostalTest()
+        {
+            Assert.IsFalse(metodo.comprobarCodigoPostal("010000"));
+            Assert.IsFalse(metodo.comprobarCodigoPostal("0100"));
+            Assert.IsFalse(metodo.comprobarCodigoPostal("99000"));
+            Assert.IsFalse(metodo.comprobarCodigoPostal("00000"));
+            Assert.IsTrue(metodo.comprobarCodigoPostal("01000"));
+            Assert.IsFalse(metodo.comprobarCodigoPostal("01A00"));
+            Assert.IsFalse(metodo.comprobarCodigoPostal(""));
+        }
+
+        [TestMethod()]
+        public void comprobarIBAN()
+        {
+            Assert.IsFalse(metodo.comprobarIBAN("ES00 00000 0000 00 0000000000 "));
+            Assert.IsFalse(metodo.comprobarIBAN("ES00 000 0000 00 0000000000"));
+            Assert.IsFalse(metodo.comprobarIBAN("00 0000 0000 00 0000000000"));
+            Assert.IsFalse(metodo.comprobarIBAN("YY08 1465 1234 46 1234567890"));
+            Assert.IsTrue(metodo.comprobarIBAN("ES08 1465 1234 46 1234567890"));
+            Assert.IsFalse(metodo.comprobarIBAN("ES99 1465 1234 46 1234567890"));
+            Assert.IsFalse(metodo.comprobarIBAN("ES00 AB00 0000 00 0000000000"));
+            Assert.IsFalse(metodo.comprobarIBAN("0000 0000 0000 00 0000000000"));
+
+        }
+
+        [TestMethod()]
+        public void comprobarIPv4()
+        {
+            Assert.IsFalse(metodo.comprobarIPv4("125255255255 "));
+            Assert.IsFalse(metodo.comprobarIPv4("125.255.255"));
+            Assert.IsFalse(metodo.comprobarIPv4("125..255.255"));
+            Assert.IsFalse(metodo.comprobarIPv4("125.2555.255.255"));
+            Assert.IsFalse(metodo.comprobarIPv4("125.255.255.256"));
+            Assert.IsFalse(metodo.comprobarIPv4("0.255.255.255"));
+            Assert.IsTrue(metodo.comprobarIPv4("125.255.255.255"));
+            Assert.IsFalse(metodo.comprobarIPv4("127.255.255.255"));
+            Assert.IsFalse(metodo.comprobarIPv4(""));
+        }
+
+        [TestMethod()]
+        public void comprobarContrasena1Test()
+        {
+            Assert.IsTrue(metodo.comprobarContrasena1("Contras55"));
+            Assert.IsFalse(metodo.comprobarContrasena1("Contra55"));
+            Assert.IsFalse(metodo.comprobarContrasena1("PASSWORD55"));
+            Assert.IsTrue(metodo.comprobarContrasena1("Password214"));
+            Assert.IsFalse(metodo.comprobarContrasena1("password343"));
+            Assert.IsTrue(metodo.comprobarContrasena1("Password1234"));
+            Assert.IsFalse(metodo.comprobarContrasena1("234235435346"));
+            Assert.IsFalse(metodo.comprobarContrasena1("PasswordWrong"));
+            Assert.IsFalse(metodo.comprobarContrasena1(""));
         }
 
         [TestMethod()]
@@ -33,7 +158,7 @@ namespace DesarrolloDirigidoPorPruebas.Tests
             Assert.IsFalse(metodo.comprobarContrasena2("Password1234"));
             Assert.IsFalse(metodo.comprobarContrasena2(""));
         }
-
+        /*
         [TestMethod()]
         public void comprobarURLTest()
         {
@@ -48,5 +173,6 @@ namespace DesarrolloDirigidoPorPruebas.Tests
             Assert.IsTrue(metodo.comprobarURL("https://ubuvirtual.ubu.es"));
             Assert.IsFalse(metodo.comprobarURL(""));
         }
+        */
     }
 }
